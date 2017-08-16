9524f8fc25a36e0f276bc6431e1688a062b1b7fb
Yonglin Sun <sunyonglincanada@gmail.com>
2017-08-15 23:58:31 -0400

    add post create and update time and format the time add and make post edit and update button work

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -3,9 +3,36 @@
 @section('title', '| View Post')
 
 @section('content')
+  <div class="row">
 
-    <h1>{{ $post->title }}</h1>
+    <div class="col-md-8">
+        <h1>{{ $post->title }}</h1>
 
-    <p class="lead">{{ $post->body }}</p>
+        <p class="lead">{{ $post->body }}</p>
+    </div>
 
+    <div class="col-md-4">
+        <div class="well">
+            <dl class="dl-horizontal">
+                <dt>Create At:</dt>
+                <dd>{{ date( 'M j, Y H:i', strtotime($post->created_at) ) }}</dd>
+            </dl>
+
+            <dl class="dl-horizontal">
+                <dt>Last Updated:</dt>
+                <dd>{{ date( 'M j, Y H:i', strtotime($post->updated_at) ) }}</dd>
+            </dl>
+            <hr>
+
+            <div class="row">
+                <div class="col-sm-6">
+                    {!! Html::linkRoute('posts.edit', 'Edit', array($post->id),array('class'=> 'btn btn-primary btn-block') ) !!}
+                </div>
+                <div class="col-sm-6">
+                    {!! Html::linkRoute('posts.destroy', 'Delete', array($post->id),array('class'=> 'btn btn-danger btn-block') ) !!}
+                </div>
+            </div>
+        </div>
+    </div>
+  </div>
 @endsection
