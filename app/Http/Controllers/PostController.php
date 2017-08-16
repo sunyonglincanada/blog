0cb45ae7a86f0fb695035c11eeb2413a42d4eec2
Yonglin Sun <sunyonglincanada@gmail.com>
2017-08-16 11:59:19 -0400

    add post index page with edit and view button

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -15,7 +15,12 @@ class PostController extends Controller
      */
     public function index()
     {
-        //
+        // create a variable and store all the blog posts in it from the database
+        $posts = Post::all();
+
+
+        // return a view and pass in the above variable
+        return view('posts.index')->withPosts($posts);
     }
 
     /**
--- resources/views/partials/_head.blade.php
+++ resources/views/partials/_head.blade.php
@@ -9,8 +9,10 @@
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
 
-        <!-- Latest compiled and minified CSS -->
+        <!-- Bootstrap -->
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+        {{ Html::style('css/styles.css') }}
 
         @yield('stylesheets')
 
