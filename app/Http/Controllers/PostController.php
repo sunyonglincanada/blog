4912eb3e9dbf8f27c7aa60bc0f84f1366b34f242
Yonglin Sun <sunyonglincanada@gmail.com>
2017-08-17 00:13:49 -0400

    save updated post data to database and minor bug fix

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -99,7 +99,25 @@ class PostController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+        // Validate the data
+        $this->validate($request, array(
+            'title' => 'required|max:255',
+            'body'  => 'required',
+        ));
+
+        // Save the data to the database
+        $post = Post::find($id);
+
+        $post->title = $request->input('title');
+        $post->body  = $request->input('body');
+
+        $post->save();
+
+        // Set flash data with success message
+        Session::flash('success','This post was successfully saved.');
+
+        // Redirect with flash data to posts.show
+        return redirect()->route('posts.show', $post->id );
     }
 
     /**
