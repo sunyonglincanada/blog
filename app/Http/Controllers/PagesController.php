d6083f915c662b4ffa72ec1eae4f1ead0c5d027f
Yonglin Sun <sunyonglincanada@gmail.com>
2017-08-19 23:13:36 -0400

    add index page query loop to show recent posts and add link to post list page button in post detail page

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -3,17 +3,19 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Post;
 
 class PagesController extends Controller
 {
     public function getIndex()
     {
+        $posts = Post::orderBy('created_at','desc')->limit(4)->get();
         # process variable date or params
         # talk to the model
         # receive from the model
         # compile or process data form the model if needed
         # pass that data to the correct view
-        return view('pages/welcome');
+        return view('pages/welcome')->withPosts($posts);
     }
 
     public function getAbout()
