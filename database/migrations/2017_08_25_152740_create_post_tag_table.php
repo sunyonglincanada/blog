af7dd35dd705aaa7862651ea38d3025c8405dfbd
Yonglin Sun <sunyonglincanada@gmail.com>
2017-08-25 11:35:50 -0400

    create tag model and database tables build connection between tas and posts

---
+++ database/migrations/2017_08_25_152740_create_post_tag_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePostTagTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('post_tag', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('post_id')->unsigned();
+            // post_id is the respresentation of id column in posts table
+            $table->foreign('post_id')->references('id')->on('posts');
+
+            $table->integer('tag_id')->unsigned();
+            // tag_id is the respresentation of id column in tags table
+            $table->foreign('tag_id')->references('id')->on('tags');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('post_tag');
+    }
+}
